4116e61344cb713a0fa6598bfa61f403212eb42e
github-ci <you@example.com>
2025-05-08 08:45:54 +0000

    fix

--- src/Entity/ServerMessage.php
+++ src/Entity/ServerMessage.php
@@ -24,7 +24,6 @@ class ServerMessage
 {
     #[ListColumn(order: -1)]
     #[ExportColumn]
-    #[Groups(['restful_read', 'api_tree', 'admin_curd', 'api_list'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: Types::INTEGER, options: ['comment' => 'ID'])]
